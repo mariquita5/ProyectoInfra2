b4b0aafdb2c1dfd9bafb2fe28689ce6b04a13e97
mariquita5 <majomadrig5@gmail.com>
2016-11-23 10:59:56 -0600

    +cambios

--- ProyectoInfra2/Vista/inicio.aspx.cs
+++ ProyectoInfra2/Vista/inicio.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace TutorCognitivoDS2.vista
+namespace ProyectoInfra2.Vista
 {
-    public partial class AdministrarDatos : System.Web.UI.Page
+    public partial class inicio : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
