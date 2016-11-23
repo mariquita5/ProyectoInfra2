bb7ec560bdf491d6a2a001416d87070c4a2829f4
mariquita5 <majomadrig5@gmail.com>
2016-11-23 10:19:08 -0600

    +cambios

---
+++ ProyectoInfra2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoInfra2.Startup))]
+namespace ProyectoInfra2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
