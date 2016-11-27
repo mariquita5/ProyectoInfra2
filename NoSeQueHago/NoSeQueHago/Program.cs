6eff9fec9a34ceb3996292303c92af96a00ab8a6
Joseph Vega <j-vv01@hotmail.com>
2016-11-26 23:46:01 -0600

    Ayuda!!

--- NoSeQueHago/NoSeQueHago/Program.cs
+++ NoSeQueHago/NoSeQueHago/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mail;
 
 namespace NoSeQueHago
 {
@@ -11,7 +12,14 @@ namespace NoSeQueHago
     {
         static void Main(string[] args)
         {
-            SmtpDirect sm = new SmtpDirect();
+            MailMessage mail = new MailMessage();
+            mail.From = "admin@infratec.com";
+            mail.To = "jvega@infratec.com";
+            mail.Subject = "Prueba";
+            mail.Body = "Hola luuuuuu";
+
+            //SmtpDirect sm = new SmtpDirect();
+            SmtpDirect.Send(mail);
         }
     }
 }
