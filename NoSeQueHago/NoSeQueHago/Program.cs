d01eb40a696b5c8e94b24bcfffc3eb90eb81bcbd
Joseph Vega <j-vv01@hotmail.com>
2016-11-26 23:17:10 -0600

    Probando el Codigo local

---
+++ NoSeQueHago/NoSeQueHago/Program.cs
@@ -0,0 +1,17 @@
+﻿using mySMTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoSeQueHago
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmtpDirect sm = new SmtpDirect();
+        }
+    }
+}
